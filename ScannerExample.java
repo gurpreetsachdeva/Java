5f6d4fac91ae03ffc5be0e3175f21cb33ce17ff2
Gurpreet <gurpreet.sachdevagss@yahoo.in>
2017-07-08 14:14:55 +0530

    Playing with Scanner Class for Inputs.

---
+++ ScannerExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ScannerExample {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		String input=scan.next();
+		scan.close();
+		System.out.println(input);
+	}
+
+
+}
